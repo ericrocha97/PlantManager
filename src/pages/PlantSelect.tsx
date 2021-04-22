87f7ac7e09f02101fa231809bda7c7bf9e9798da
Eric Rocha <ericrochaeric@gmail.com>
2021-04-22 04:29:10 -0300

    Update PlantSelect.tsx

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -134,6 +134,7 @@ export function PlantSelect() {
       <View>
         <FlatList
           data={environments}
+          keyExtractor={item => String(item.key)}
           renderItem={({ item }) => (
             <EnvironmentButton
               title={item.title}
@@ -152,6 +153,7 @@ export function PlantSelect() {
       <View style={styles.plants}>
         <FlatList
           data={filteredPlants}
+          keyExtractor={item => String(item.id)}
           renderItem={({ item }) => (
             <PlantCardPrimary data={item} />
           )}
